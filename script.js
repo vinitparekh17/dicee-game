c4c327287186c5c9a8b0d7c31597006f22c1064e
Vinit Parekh <77158947+vinitsoni17@users.noreply.github.com>
2021-10-29 13:02:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const arr = ["./images/dice1.png", "./images/dice2.png", "./images/dice3.png", "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"]
+
+var result1 = arr[Math.floor(Math.random() * 6)]
+
+var result2 = arr[Math.floor(Math.random() * 6)]
+
+document.querySelectorAll("img")[0].setAttribute("src", result1)
+
+document.querySelectorAll("img")[1].setAttribute("src", result2)
+
+var won = null
+
+if(result1 > result2) {
+    won = "player 1 is won"
+} else if(result1 === result2) {
+    won = "Match draw"
+} else {
+    won = "player 2 is won"
+}
+document.querySelector("h1").innerHTML = won
